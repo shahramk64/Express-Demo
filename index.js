41e207667518ecd4c71a7d1c790e04f31cae9ac3
Shahram Kalantari <skalantari@fortherecord.com>
2018-06-08 15:41:59 +1000

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 
 const app = express();
-
+const courses=[
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'}
+];
 app.get('/', function (req, res)
 {
     res.send('hello world');
@@ -10,10 +14,21 @@ app.get('/', function (req, res)
 
 app.get('/api/courses', function (req,res)
 {
-    res.send([1,2,3]);
+    res.send(courses);
 })
 
-app.listen(3000, function()
+app.get('/api/courses/:id', function (req,res)
 {
-    console.log('listening to port 3000...');
+    const c = courses.find(c => c.id == parseInt(req.params.id));
+    if(!c)
+        res.status(404).send('The course with that Id was not found!');
+    else
+        res.send(c);
+
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function()
+{
+    console.log(`listening to port ${port}...`, port);
 });
